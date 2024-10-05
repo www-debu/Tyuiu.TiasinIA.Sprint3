85f9338cc206b091bae017c2d29a2cbb04c29650
Илья Тясин <turbopad7200@gmail.com>
2024-10-05 21:59:43 +0500

    Task7

--- Tyuiu.TiasinIA.Sprint3.Task7.V15/Program.cs
+++ Tyuiu.TiasinIA.Sprint3.Task7.V15/Program.cs
@@ -38,15 +38,15 @@ namespace Tyuiu.TiasinIA.Sprint3.Task7.V15
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("+----------+-----------+");
-            Console.WriteLine("|    X     |   f(x)    |");
-            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("+----------+------------+");
+            Console.WriteLine("|    X     |   f(x)     |");
+            Console.WriteLine("+----------+------------+");
             for (int i = 0; i <= len - 1; i++)
             {
-                Console.WriteLine("|{0,5:d}     |  {1, 5:f2}    |", startValue, valueArray[i]);
+                Console.WriteLine("|{0,5:d}     |  {1, 6:f2}    |", startValue, valueArray[i]);
                 startValue++;
             }
-            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("+----------+------------+");
             Console.ReadKey();
         }
     }
