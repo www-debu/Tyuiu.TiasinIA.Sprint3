95757b071c77e16d9567cf1492344beeb3893a12
Илья Тясин <turbopad7200@gmail.com>
2024-10-05 21:13:13 +0500

    Task6

---
+++ Tyuiu.TiasinIA.Sprint3.Task6.V23.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.TiasinIA.Sprint3.Task6.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.TiasinIA.Sprint3.Task6.V23/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.TiasinIA.Sprint3.Task6.V23.Lib;
+namespace Tyuiu.TiasinIA.Sprint3.Task6.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Тясин И. А. | АСОиУб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Оператор цикла for                                                *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Тясин Илья Александрович | АСОиУб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=sin(x)/cos(x)\r\n\r\nПри х = 0 прервать цикл.                 *");
+            Console.WriteLine("* Полученные значения перемножать.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 18;
+            int stopValue = 28;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey(); ;
+        }
+    }
+}
