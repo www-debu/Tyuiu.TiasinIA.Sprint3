b07ba8b7be80cc116fc99822519b4992f7706ade
Илья Тясин <turbopad7200@gmail.com>
2024-10-15 16:50:05 +0500

    Task6

--- Tyuiu.TiasinIA.Sprint3.Task6.V23.Lib/DataService.cs
+++ Tyuiu.TiasinIA.Sprint3.Task6.V23.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.TiasinIA.Sprint3.Task6.V23.Lib
 {
-    public class DataService : ISprint3Task6V20
+    public class DataService : ISprint3Task6V23
     {
         public int GetSumTheDivisors(int startValue, int stopValue)
         {
